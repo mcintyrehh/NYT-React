3fd1f5dca1bbe7a6604aa2c07fb048a01c06ac71
Onray <mcintyrehh@gmail.com>
2018-11-04 21:13:21 -0500

    adding heroku build script

---
+++ nyt-react-app/client/build/precache-manifest.7b80f0a2f28869f06699a70e8f7d1612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e741e4d1e5d63aefae6e",
+    "url": "/static/js/main.e741e4d1.chunk.js"
+  },
+  {
+    "revision": "267dd7d46e103df4c40f",
+    "url": "/static/js/1.267dd7d4.chunk.js"
+  },
+  {
+    "revision": "e741e4d1e5d63aefae6e",
+    "url": "/static/css/main.c4dc1567.chunk.css"
+  },
+  {
+    "revision": "267dd7d46e103df4c40f",
+    "url": "/static/css/1.a6d60be7.chunk.css"
+  },
+  {
+    "revision": "331b4f949ee284cfe9b74f32262c5a3c",
+    "url": "/index.html"
+  }
+];
